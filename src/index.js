ae259762ceee3a47f224ceffcc4e9d1d971db69a
Marlon Munoz <marlon@marlonshade.com>
2024-07-19 16:09:56 -0400

    mini long lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTaskForm = document.getElementById('create-task-form')
+
+  createTaskForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const tasksList = document.getElementById('tasks')
+    const taskLI = document.createElement('li')
+    const newTaskDescriptionInputElement = document.getElementById('new-task-description')
+    taskLI.append(newTaskDescriptionInputElement.value + " ")
+    const deleteTaskButton = document.createElement('button')
+    deleteTaskButton.textContent = "X"
+    deleteTaskButton.setAttribute('data-description', newTaskDescriptionInputElement.value)
+    deleteTaskButton.addEventListener('click', () => {
+      taskLI.remove()
+    })
+
+    taskLI.append(deleteTaskButton)
+    tasksList.appendChild(taskLI)
+    createTaskForm.reset()
+  })
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+``
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
